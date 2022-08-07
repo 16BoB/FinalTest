978d17c773704d8f379c1b7e70b48be3e79861f5
Tape <trid.skivi@gmail.com>
2022-08-07 16:29:57 +0300

    added documentation to methods

--- Program.cs
+++ Program.cs
@@ -1,12 +1,12 @@
 ﻿string[] inputArray = { "hello", "2", "world", ":-)", "1234", "1567" };
 
-// Медод для мечати массива
+// Method for Printing an Array
 string PrintArray(string[] inputArray)
 {
     return string.Join(", ", inputArray);
 }
 
-// Метод для подсчета строк в массиве длинной меньше или равных 3 символов
+// Method for counting strings in an array less than or equal to 3 characters long
 int CountStringInArray(string[] input)
 {
     int len = inputArray.Length;
@@ -22,8 +22,8 @@ int CountStringInArray(string[] input)
     return count;
 }
 
-// Метод для создания нового массива из строк длинной меньше или равных 3 символов, которые найдет во входном массиве
-// ! в аргумент countWords передаем результат работы метода CountStringInArray !
+// Method for creating a new array from strings less than or equal to 3 characters that it finds in the input array
+// ! pass the result of the CountStringInArray method to the countWords argument !
 string[] CreateNewArray(string[] inputArray, int countWords)
 {
     int len = inputArray.Length;
