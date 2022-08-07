0a9c6c600955dada7a72e1e046362db229124afe
Tape <trid.skivi@gmail.com>
2022-08-07 15:50:04 +0300

    outlined the necessary methods

--- Program.cs
+++ Program.cs
@@ -1,1 +1,18 @@
 ﻿string[] inputArray = { "hello", "2", "world", ":-)", "1234", "1567" };
+
+// 
+string PrintArray(string[] inputArray)
+{
+    string result = string.Empty;
+
+    return result;
+}
+
+// 
+string[] FindStringInArrAndPushInNewArr(string[] inputArray)
+{
+    int len = inputArray.Length;
+    string[] outputArray = new string[len];
+
+    return outputArray;
+}
